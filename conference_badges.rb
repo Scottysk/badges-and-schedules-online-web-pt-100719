cb2c10504aec60a42ee4ef932979c521f24ba128
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-13 23:05:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,9 +3,6 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees = []
-  attendees.each do |name|
-    attendees << badge_maker(name)
-  end
-  return attendees
+  attendees.collect do |name|
+    badge_maker(name)
 end
