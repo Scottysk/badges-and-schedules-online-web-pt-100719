17c669fb96679cd12811704db63f2c216e072784
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-13 22:49:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,5 +4,7 @@ end
 
 def batch_badge_creator(attendees)
   attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  attendees.each do |badges|
+    return "Hello, my name is #(badges)"
   
 end
