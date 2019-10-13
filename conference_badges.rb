caafcc3e23961f9722ba0765108f2d71850c8db9
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-13 22:44:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,4 @@
 def badge_maker
-  badges = ["Edsger"""""""""""", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  badges = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   puts "Hello, my name is #{name}"
 end
