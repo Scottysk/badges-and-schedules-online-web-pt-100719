df1af6eb1b0472021ae4a669ae9c8a652f758c1a
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-13 23:19:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ end
 
 def assign_rooms(attendees)
   attendee = []
-  room = 0
+  room = 1
   attendees.each do |name|
     attendee << "Hello, #{name}! You'll be assigned to room #{room}!"
       room += 1
